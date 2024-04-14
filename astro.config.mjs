72e4b41ba9c58e65482ec3cd5d352447b93a5ae3
EveSunMaple <evesunmaple@outlook.com>
2024-04-14 11:21:30 +0800

    add rehypeKatex

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,15 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import tailwind from "@astrojs/tailwind";
+import remarkMath from 'remark-math';
+import rehypeKatex from 'rehype-katex';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
-	integrations: [mdx(), sitemap(), tailwind()]
+	site: 'https://www.saroprock.com',
+	integrations: [mdx(), sitemap(), tailwind()],
+	markdown: {
+	  remarkPlugins: [remarkMath],
+	  rehypePlugins: [rehypeKatex]
+	},
 });
--- src/content/config.ts
+++ src/content/config.ts
@@ -6,10 +6,9 @@ const blog = defineCollection({
 	schema: z.object({
 		title: z.string(),
 		description: z.string(),
-		// Transform string to Date object
 		pubDate: z.coerce.date(),
-		updatedDate: z.coerce.date().optional(),
-		heroImage: z.string().optional(),
+		updated: z.coerce.date().optional(),
+		image: z.string().optional(),
 		badge: z.string().optional(),
 		tags: z.array(z.string()).refine(items => new Set(items).size === items.length, {
 			message: 'tags must be unique',
