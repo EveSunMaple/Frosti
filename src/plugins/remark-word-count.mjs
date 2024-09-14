f84be448154fdc7c6fa91b6557b77d1120f64005
Loping151 <97866915+Loping151@users.noreply.github.com>
2024-09-15 04:08:32 +0800

    fix wordcount

--- src/plugins/remark-word-count.mjs
+++ src/plugins/remark-word-count.mjs
@@ -3,7 +3,10 @@ import { toString } from 'mdast-util-to-string';
 export function remarkWordCount() {
     return function (tree, { data }) {
         const textOnPage = toString(tree);
-        const wordCount = textOnPage.split(/\s+/).filter(Boolean).length;
+        const wordCount = textOnPage
+            .replace(/[\u4e00-\u9fa5]/g, ' $& ')
+            .split(/\s+/)
+            .filter(Boolean).length;
         data.astro.frontmatter.wordCount = wordCount;
     };
 }
